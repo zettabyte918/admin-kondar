72c05937b1c1b0af16264da4666bc7d5cc8cf61e
Hammami Houssem <hammamihossem99@gmail.com>
2022-09-25 14:14:10 +0100

    update title

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -41,7 +41,7 @@ export default function Signin() {
   return (
     <>
       <Head>
-        <title>connectez-vous || LES EXPERTS</title>
+        <title>connectez-vous || KONDAR SCHOOL</title>
       </Head>
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
